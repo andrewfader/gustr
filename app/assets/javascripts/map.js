275fc3967635299f15613a473a636f9d7b35461c
Andrew Fader <andrevan@gmail.com>
2013-10-31 14:20:54 -0400

    switch to cloudmade

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -8,8 +8,8 @@ function readyUp() {
       lng = position.coords.longitude;
       var map = L.map('map').setView([lat, lng], 16);
 
-      L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      L.tileLayer('http://{s}.tile.cloudmade.com/babee756d2484307ba907bd611d0a64c/997/256/{z}/{x}/{y}.png', {
+        // attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
       }).addTo(map);
       L.marker([lat, lng]).addTo(map)
       .bindPopup('A pretty CSS3 popup. <br> Easily customizable.')
