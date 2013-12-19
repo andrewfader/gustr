e5b2d1ee7b28695341dc45fd5f6aaad40cf36622
Andrew Fader <andrevan@gmail.com>
2013-12-18 23:06:13 -0500

    minor tweak

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,6 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require jquery.ui.all
 //= require autocomplete-rails
 //= require turbolinks
 //= require_tree ../../../vendor/assets/javascripts
