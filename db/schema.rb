56456a2eaa4158c3001d70b4fd738cebeb1767e6
Andrew Fader <andrevan@gmail.com>
2014-02-15 15:54:32 -0500

    fixes

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140207015115) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "businesses", force: true do |t|
     t.string   "name"
     t.datetime "created_at"
@@ -102,7 +105,7 @@ ActiveRecord::Schema.define(version: 20140207015115) do
     t.string   "image_url"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
