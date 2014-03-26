05fa164971fe85aff0d59395e68d037144deb8a9
Andrew Fader <andrevan@gmail.com>
2014-03-25 20:23:12 -0400

    fix slideshow

--- app/assets/javascripts/ppt.js
+++ app/assets/javascripts/ppt.js
@@ -1,22 +1,25 @@
-$(document).ready(function() {
-  if(document.location.toString().indexOf('welcome') != 0) {
+function gusto() {
+
+  if(document.location.toString().indexOf('welcome') != -1) {
     function redir(string) {
       setTimeout(function() {
-        alert(document.location.toString().split('/')[2])
-        window.location.href = string;
+        var url = 'http://' + document.location.toString().split('/')[2] + '/' + string;
+        Turbolinks.visit(url);
       }, 3000);
     }
-    if(document.location.toString().indexOf('1') != 0) {
-        redir('/welcome2');
+    if(document.location.toString().indexOf('1') != -1) {
+        redir('welcome2');
     }
-    else if(document.location.toString().indexOf('2') != 0) {
-        redir('/welcome');
+    else if(document.location.toString().indexOf('2') != -1) {
+        redir('welcome');
     }
 
     else {
-        redir('/welcome1');
+        redir('welcome1');
 
     }
   }
 
-} );
+}
+$(document).ready(function() { gusto(); } );
+$(document).on('page:load', function() { gusto(); });
