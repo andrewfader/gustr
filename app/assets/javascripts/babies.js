bcbc51d353285ec53286bff8eb06e21f370d33fd
Andrew Fader <andrevan@gmail.com>
2014-06-24 18:51:52 -0400

    text editor

--- app/assets/javascripts/babies.js
+++ app/assets/javascripts/babies.js
@@ -1,3 +1,15 @@
+var teAlready;
+function showTe() {
+    if(window.teAlready == undefined) {
+      $('.floater textarea').show();
+      $('.floater .ok').show();
+      $('.floater textarea').jqte();
+      $('.floater textarea').jqteVal($('.floater .caption').val());
+      $('.floater .caption').hide();
+      window.teAlready = 1;
+      $('a.jqte_tool_label').click();
+    }
+}
 function bindEvents() {
   var arr = window.location.toString().split('/');
   var url = 'http://' + arr[2] + '/images/' + arr[4];
@@ -33,6 +45,7 @@ function bindEvents() {
       $('.floater').css('left', data.posX);
     }
 
+    $('.floater').show();
   });
   $('.floater').draggable({
     stop: function() {
@@ -52,6 +65,26 @@ function bindEvents() {
       })
     }
   })
+
+  $('.ok').click(function() {
+    if (window.teAlready = 1) {
+      $.ajax({
+        type: "PUT",
+        url: url + '.json',
+        data: {image: { caption: $('.floater textarea').val() }}
+      });
+      $('.floater .caption').html($('.floater textarea').val());
+      $('.floater textarea').jqte();
+      $('.floater .caption').show();
+      $('.floater .ok').hide();
+    }
+  });
+  $('.ok').button();
+  $('.floater').click(function() { showTe(); });
+  if($('.floater .caption').text() == "") {
+    showTe();
+  }
 }
+
 $(document).ready(function() { bindEvents(); } )
 $(document).on('page:load', function() { bindEvents(); } )
