143d39a4643346080faa9b1a7eeddb4d58d3a864
Andrew Fader <andrevan@gmail.com>
2014-09-16 16:20:11 -0400

    wip

--- app/assets/javascripts/babies.js
+++ app/assets/javascripts/babies.js
@@ -100,6 +100,20 @@ function bindEvents() {
   $('.floater').click(function() { showTe(); });
   if($.trim($('.floater .caption').text() == "")) {
   }
+
+  var completer;
+  input = document.getElementById('image_place_name');
+  autocomplete = new google.maps.places.Autocomplete(input);
+  autocomplete.setTypes(['establishment']);
+
+  google.maps.event.addListener(autocomplete, 'place_changed', function() {
+    var place = autocomplete.getPlace();
+    var address = '';
+    if (place.address_components) {
+      address = [ (place.address_components[0] && place.address_components[0].short_name || ''), (place.address_components[1] && place.address_components[1].short_name || ''), (place.address_components[2] && place.address_components[2].short_name || '') ].join(' ');
+    }
+    alert(address)
+  });
 }
 
 $(document).ready(function() { bindEvents(); } )
