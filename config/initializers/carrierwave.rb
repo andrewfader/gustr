77b91daad2b1e3370b9bd935f7a492eef304a5cc
Andrew Fader <andrevan@gmail.com>
2018-10-11 21:48:26 -0400

    Delete carrierwave.rb

--- config/initializers/carrierwave.rb
+++
@@ -1,9 +0,0 @@
-require 'carrierwave/orm/activerecord'
-CarrierWave.configure do |config|
-  config.fog_credentials = {
-    :provider               => 'AWS',
-    :aws_access_key_id      => 'AKIAIE5N4T5344BWDO2Q',
-    :aws_secret_access_key  => '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',
-  }
-  config.fog_directory  = 'gustr'
-end
